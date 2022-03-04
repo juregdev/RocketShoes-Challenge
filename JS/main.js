8a38eb77d24735ee09284e99c0edfdc175b4be43
Filipe Barros <filipe.araujo9@outlook.com>
2022-03-04 19:29:37 -0300

    update
    Update ./CSS/style.css, ./JS/main.js and index.html
    
    Fixed Responsivity

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+function dropMenu(){
+  const menu= document.querySelector("#menuMobile")
+  const line1= document.querySelector("#line1")
+  const line2=document.querySelector("#line3")
+  const body=document.querySelector("body")
+
+  if(menu.style.transform == "translateX(0px)"){
+     menu.style.transform ="translateX(110%)"
+    line1.style.transform='rotate(0deg)'
+    line2.style.transform='rotate(0deg)'
+    line2.style.top='0'
+    line1.style.top='0'
+    body.style.overflow = "visible"
+        console.log(line1)
+  }
+
+  else{
+    menu.style.transform ="translateX(0)"
+    line1.style.transform='rotate(45deg)'
+    line2.style.transform='rotate(-45deg)' 
+    line1.style.top='0.6rem' 
+    line2.style.top='-0.7rem'
+    body.style.overflow = "hidden"
+
+  }
+
+  
+
+}
